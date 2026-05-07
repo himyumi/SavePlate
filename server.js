14cb7f15c04f424f1d9ce91f431761bc6f63a0bb
himyumi <jasonaldrearis@gmail.com>
2026-05-08 01:01:23 +0800

    new mongoDB works now

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 // Schemas
 const userSchema = new mongoose.Schema({
