22535c29d949962ff13094e2235cab4f3b181202
LeftenantLungs <daffytheduck616@gmail.com>
2026-05-18 11:20:50 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,14 +5,24 @@
    Project: saveplate-64df4
    ================================================================ */
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:            "AIzaSyAIZ-okljSJbqDS0PoXdOI7D0Hr403K0o8",
-  authDomain:        "saveplate-64df4.firebaseapp.com",
-  projectId:         "saveplate-64df4",
-  storageBucket:     "saveplate-64df4.firebasestorage.app",
-  messagingSenderId: "993911292635",
-  appId:             "1:993911292635:web:9da9e1f12cbb2227a6c2fe",
-  measurementId:     "G-RSWC83QKX8"
+  apiKey: "AIzaSyCh_V-DFTXG1oMlAOVlyhKXJLDmiH_XfMc",
+  authDomain: "saveplate-81-85-89.firebaseapp.com",
+  projectId: "saveplate-81-85-89",
+  storageBucket: "saveplate-81-85-89.firebasestorage.app",
+  messagingSenderId: "215352682694",
+  appId: "1:215352682694:web:8db6e8e37ba863e2efcfc7",
+  measurementId: "G-79EZ567774"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
